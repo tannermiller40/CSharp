722b7c275a363ae012ac4727b448ef8bd8d21559
tannermiller40 <emailaddress@yahoo.com>
2021-09-02 19:47:51 -0400

    covid helper

---
+++ CovidHelper/Helper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace CovidHelper
+{
+    public class Helper
+    {
+        // variables for helper class
+
+        public string choice = "";
+        private string name;
+        private decimal temperature;
+        private string symptoms;
+        private string exposure;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+
+
+        }
+        public decimal Temperature
+        {
+            get { return temperature; }
+            set {
+                    temperature = value;
+            }
+        }
+        public string Symptoms
+        {
+            get { return symptoms; }
+            set { symptoms = value; }
+        }
+        public string Exposure
+        {
+            get { return exposure; }
+            set { exposure = value; }
+        }
+
+        // function that converts c to f 
+
+        public string CToF(decimal temp, string choice)
+        {
+            decimal fahrenheit = 0m;
+            decimal celsius = 0m;
+            string W = "";
+            ;
+
+            // conditional to check whether user is using c or f
+
+            if(choice == "C" || choice == "c")
+            {
+                // function to convert c to f
+                fahrenheit = (temp * 9) / 5 + 32;
+                W = $"Your temperature is: " + fahrenheit + " " + "F.";
+                return W;
+            }
+            else
+            {
+                // function to convert f to c
+                celsius = (temp - 32) * 5 / 9;
+                W = $"Your temperature is: " + celsius + " " +  "C.";
+                return W;
+            }
+        }
+
+
+    }
+}
+
---
+++ CovidHelper/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace CovidHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // create a greet to the user at the beginning of the program.
+
+            Helper name1 = new Helper();
+
+            decimal temp = name1.Temperature;
+
+            Console.WriteLine(temp);
+
+
+            Console.WriteLine("What is your name?");
+            name1.Name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name1.Name);
+
+            // Asking user for last reported temperature
+
+            Console.WriteLine("What is your last recorded body temperature?");
+            name1.Temperature = decimal.Parse(Console.ReadLine());
+            
+            if (name1.Temperature > 110m)
+            {
+                Console.WriteLine("Please enter a valid temperature");
+            } 
+
+            // Creating a function to check for Celsius or Fahrenheit
+
+            Console.WriteLine("C or F?");
+            name1.choice = Console.ReadLine();
+            if (name1.Temperature > 98.6m)
+            {
+                Console.WriteLine($"Your temperature is: " + name1.Temperature + " " + name1.choice + ", you are experiencing a fever.");
+            }
+            else
+            {
+                Console.WriteLine($"Your temperature is: " + name1.Temperature + " " + name1.choice + ".");
+
+            }
+
+            // calling function converting C to F
+            // calling function as string so that I can return the opposite value with either C or F following the returned temp.
+
+            string test = name1.CToF((name1.Temperature), name1.choice);
+            Console.WriteLine(test);
+
+
+            /* have you been in contact with covid Y OR N
+               are you expeierncing any coughing Y OR N
+               are you experiencing any bodyaches Y OR N
+            */
+
+
+            /* Implementing a counter to determine if the user should contact the health department or are most likely fine.
+             * Also placing ("Y" or "N") in the question because the user needs to enter either value Y or N.
+            */
+
+            int counter = 0;
+            Console.WriteLine("Have you been in contact with anyone who has experienced symptoms or tested positive for Covid-19? (Y or N)");
+            string S = Console.ReadLine();
+            if (S == "Y" || S == "y")
+            {
+                counter++;
+            }
+            Console.WriteLine("Are you Exhibiting any symptoms of Covid-19? (Y or N)");
+            string V = Console.ReadLine();
+            if (V == "Y" || V == "y")
+            {
+                counter++;
+            }
+            Console.WriteLine("Are you experiencing any body aches or cough? (Y or N)");
+            string M = Console.ReadLine();
+            if (M == "Y" || M == "y")
+            {
+                counter++;
+            }
+
+            if (counter >= 1)
+            {
+                Console.WriteLine("You are exhibiting some signs of indication that you may have Covid-19, you should contact the health department.");
+            }
+            else
+            {
+                Console.WriteLine("No signs of Covid-19, however if you begin to feel any of these symptoms stay home and contact the health department.");
+
+            }
+
+        }
+
+    }
+}
